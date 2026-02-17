4a56db1353d9fe2b5ae759506ff078b646467f33
reydonnell12 <programacion.expert.rey@gmail.com>
2026-02-16 21:21:32 -0500

    correcion en la url

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://willowy-alpaca-5c6fb2.netlify.app/",
+      "https://willowy-alpaca-5c6fb2.netlify.app",
     ],
   }),
 );
